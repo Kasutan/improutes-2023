db2b2d5c0f2828c74f1f94ec1ded159e4878d665
Kasutan <contact@kasutan.pro>
2023-10-25 17:33:01 +0200

    topbar avec sélecteur de langue et recherche

--- inc/site-header.php
+++ inc/site-header.php
@@ -1,17 +1,41 @@
 <?php
 add_action('tha_header_top','kasutan_header_top');
 function kasutan_header_top() {
-	if(!function_exists('get_field')) {
-		return;
-	}
-	$lien=get_field('improutes_acces_client','option');
-	if(empty($lien)) {
-		return;
-	}
-	printf('<a href="%s" class="acces-client" target="%s" rel="noopener noreferrer">%s</a>',
-		esc_url($lien['url']),
-		esc_attr($lien['target']),
-		wp_kses_post( $lien['title'] )
-	);
+	echo '<div class="topbar">';
 
+	$aria="Formulaire de recherche dans l\'en-tête";
+	$label="Saisissez vos mots-clés";
+	$submit="Déclencher la recherche";
+	$placeholder="Rechercher";
+	if(KPLL && pll_current_language()=='en') {
+		$aria="Search form in header";
+		$label="Type your search keywords";
+		$submit="Trigger search";
+		$placeholder="Search";
+	}
+
+	printf('<form role="search" method="get" class="search-form" action="/" aria-label="%s">
+			<label>
+				<span class="screen-reader-text">%s</span>
+				<input class="search-field" 
+				placeholder="%s" value="" name="s" type="search"></label>
+			<input class="search-submit screen-reader-text" value="%s" type="submit">
+		</form>',
+		$aria,
+		$label,
+		$placeholder,
+		$submit
+	);
+	
+
+	if(KPLL) {
+		echo '<ul class="selecteur">';
+		pll_the_languages(array(
+			'show_flags'=> 1,
+			'hide_current'=>1
+		));
+		echo '</ul>';
+	}
+	
+	echo '</div>';
 }
